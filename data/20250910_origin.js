b901819fb631ed9ed5c70f3440b8a8ac944a3339
yu.kuai <yu.kuai@chaitin.com>
2025-09-10 17:59:08 +0800

    feat: 910完成

---
+++ data/20250910_origin.js
@@ -0,0 +1,122 @@
+const data = [
+  {
+    "img": "banner_1",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0807/45686/index.html#/"
+  },
+  {
+    "img": "banner_2",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0731/54705/index.html#/"
+  },
+  {
+    "img": "banner_3",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/kct/02/index.html#/"
+  },
+  {
+    "img": "banner_4",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/kct/01/index.html#/"
+  },
+  {
+    "img": "banner_5",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/12/index.html#/"
+  },
+  {
+    "img": "banner_6",
+    "title": "9-10月课程资源征集通知",
+    "href": "https://age06.edu.sh.cn/age06web/home/content/2025/7368522496175247360.html?setOfId=6938&setOfId=6938"
+  },
+  {
+    "img": "topic_left",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/05/index.html#/"
+  },
+  {
+    "img": "topic_right_1",
+    "title": "数智赋能，让运动更科学",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/242e72f8-626f-47c0-9d45-f5ff7307c89a"
+  },
+  {
+    "img": "topic_right_2",
+    "title": "“智慧地图”在幼儿户外运动实施中的运用",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2ea180d1-bbe1-4133-a084-9978cf835fa6"
+  },
+  {
+    "img": "life_1",
+    "title": "小班生活活动：爱上幼儿园",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/88e24b19-997f-4621-bdc4-2fef1ddafc6c"
+  },
+  {
+    "img": "life_2",
+    "title": "中班生活活动：我的名字",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/8ce86c09-b138-4c31-9f5e-9a90acce240c"
+  },
+  {
+    "img": "life_3",
+    "title": "大班生活活动：值日生",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6d9c2215-6207-431a-9c80-6bd66b8b948f"
+  },
+  {
+    "img": "sport_1",
+    "title": "小班活动案例：有趣的小路",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9cce9e5a-3351-4cb6-8d86-a1e759d427bf"
+  },
+  {
+    "img": "sport_2",
+    "title": "中班区域性运动：竹的创玩",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/163bcb66-e8cf-4ab3-b4ca-61e8b8a46b5a"
+  },
+  {
+    "img": "sport_3",
+    "title": "大班运动活动：保护彩旗",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/8163bd80-efcb-43a4-b244-32eca22e7772"
+  },
+  {
+    "img": "learn_1",
+    "title": "小班社会：讲礼貌",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/42ff9c49-f59d-4aae-9dcc-6b2b77986a5c"
+  },
+  {
+    "img": "learn_2",
+    "title": "中班语言活动：跑跑镇",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a1e2f9c2-c26f-482d-a3cc-16819dc17a6c"
+  },
+  {
+    "img": "learn_3",
+    "title": "大班学习活动：我是中国人",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/52988e41-9be6-49cc-84a8-ddeaff5a5a06"
+  },
+  {
+    "img": "game_1",
+    "title": "小班游戏活动：娃娃家",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/22539136-1f0a-48aa-9db1-3e160d80e191"
+  },
+  {
+    "img": "game_2",
+    "title": "中班游戏活动：梦立方",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/39898929-b580-4c2b-9594-d11578e1c127"
+  },
+  {
+    "img": "game_3",
+    "title": "大班建构游戏：“车船”升级记",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/4001d032-e354-4565-8b5d-c9170cc05ea0"
+  },
+  {
+    "img": "material_1",
+    "title": "主题素材包：四季之四季特征（秋）",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6b674797-e468-4015-87ed-8cf9d2350b8b"
+  },
+  {
+    "img": "material_2",
+    "title": "主题素材包：中国美景之大兴安岭",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e846c44f-f4b8-49a5-9373-863c56caf8a8"
+  },
+  {
+    "img": "material_3",
+    "title": "主题素材包：各种各样的交通工具之飞机",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/79a67c86-f77a-483f-bf18-b4a67ae3cfc1"
+  }
+];
--- script/file.sh
+++ script/file.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "错误: 需要提供一个参数"
-    echo "用法: $0 <参数1>"
-    exit 1
-fi
+# if [ $# -ne 1 ]; then
+#     echo "错误: 需要提供一个参数"
+#     echo "用法: $0 <参数1>"
+#     exit 1
+# fi
 
 DATE_FORMAT=$(date +"%Y%m%d")
 ORIGIN_FILENAME="./data/${DATE_FORMAT}_origin.js"
@@ -13,10 +13,10 @@ NEW_FILENAME="./data/${DATE_FORMAT}_replace.js"
 cp "./data/origin.js" "$ORIGIN_FILENAME"
 echo "\n🍎 文件 origin.js 已重命名"
 
-rm "./data/new_data.js"
-cp "$1" "./data/new_data.js"
-mv "$1" "$NEW_FILENAME"
-echo "\n🍎 文件 ${1} 已复制并重命名"
+# rm "./data/new_data.js"
+# cp "$1" "./data/new_data.js"
+# mv "$1" "$NEW_FILENAME"
+# echo "\n🍎 文件 ${1} 已复制并重命名"
 
 echo "\n🍎 开始执行合并流程..."
 node ./script/index.js
