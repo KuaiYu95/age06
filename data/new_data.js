50f291c7df00dbb46577bdff40b86b865972a34f
yu.kuai <yu.kuai@chaitin.com>
2025-09-10 17:57:48 +0800

    feat: 910 更新数据

--- data/new_data.js
+++ data/new_data.js
@@ -1,92 +1,92 @@
 const data = [
   {
-    img: 'banner_6',
-    title: '9-10月课程资源征集通知',
-    href: 'https://age06.edu.sh.cn/age06web/home/content/2025/7368522496175247360.html?setOfId=6938&setOfId=6938',
+    img: 'banner_4',
+    title: '',
+    href: 'https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0908/57441/index.html',
+  },
+  {
+    img: 'topic_left',
+    title: '',
+    href: 'https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/kct/01/index.html#/',
   },
   {
     img: 'topic_right_1',
-    title: '数智赋能，让运动更科学',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/242e72f8-626f-47c0-9d45-f5ff7307c89a',
+    title: '',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/296e2e42-5f53-4f25-a695-6594402ac848',
   },
   {
     img: 'topic_right_2',
-    title: '“智慧地图”在幼儿户外运动实施中的运用',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2ea180d1-bbe1-4133-a084-9978cf835fa6',
-  },
-  {
-    img: 'life_1',
-    title: '小班生活活动：爱上幼儿园',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/88e24b19-997f-4621-bdc4-2fef1ddafc6c',
-  },
-  {
-    img: 'life_2',
-    title: '中班生活活动：我的名字',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/8ce86c09-b138-4c31-9f5e-9a90acce240c',
-  },
-  {
-    img: 'life_3',
-    title: '大班生活活动：值日生',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6d9c2215-6207-431a-9c80-6bd66b8b948f',
-  },
-  {
-    img: 'sport_1',
-    title: '小班活动案例：有趣的小路',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9cce9e5a-3351-4cb6-8d86-a1e759d427bf',
-  },
-  {
-    img: 'sport_2',
-    title: '中班区域性运动：竹的创玩',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/163bcb66-e8cf-4ab3-b4ca-61e8b8a46b5a',
-  },
-  {
-    img: 'sport_3',
-    title: '大班运动活动：保护彩旗',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/8163bd80-efcb-43a4-b244-32eca22e7772',
-  },
-  {
-    img: 'learn_1',
-    title: '小班社会：讲礼貌',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/42ff9c49-f59d-4aae-9dcc-6b2b77986a5c',
-  },
-  {
-    img: 'learn_2',
-    title: '中班语言活动：跑跑镇',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a1e2f9c2-c26f-482d-a3cc-16819dc17a6c',
-  },
-  {
-    img: 'learn_3',
-    title: '大班学习活动：我是中国人',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/52988e41-9be6-49cc-84a8-ddeaff5a5a06',
-  },
-  {
-    img: 'game_1',
-    title: '小班游戏活动：娃娃家',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/22539136-1f0a-48aa-9db1-3e160d80e191',
-  },
-  {
-    img: 'game_2',
-    title: '中班游戏活动：梦立方',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/39898929-b580-4c2b-9594-d11578e1c127',
-  },
-  {
-    img: 'game_3',
-    title: '大班建构游戏：“车船”升级记',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/4001d032-e354-4565-8b5d-c9170cc05ea0',
-  },
-  {
-    img: 'material_1',
-    title: '主题素材包：四季之四季特征（秋）',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6b674797-e468-4015-87ed-8cf9d2350b8b',
+    title: '',
+    href: 'https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/05/index.html#/',
   },
   {
     img: 'material_2',
-    title: '主题素材包：中国美景之大兴安岭',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e846c44f-f4b8-49a5-9373-863c56caf8a8',
+    title: '主题素材包：植物的生长条件',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/c0ae0fd7-5252-416e-887c-76fcb0115c87',
   },
   {
     img: 'material_3',
-    title: '主题素材包：各种各样的交通工具之飞机',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/79a67c86-f77a-483f-bf18-b4a67ae3cfc1',
-  }
+    title: '主题素材包：天气现象之雨',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/3ef3f373-b997-40b1-92d2-2fab63c7e6ad',
+  },
+  {
+    img: 'life_1',
+    title: '小班生活活动：老师在一起',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fb8c233e-95f4-45ff-864e-8bcd88e25654',
+  },
+  {
+    img: 'life_2',
+    title: '中班生活：制定游戏规则',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/f9a3cc39-1f65-41b4-a76a-b90069c9ee3b',
+  },
+  {
+    img: 'life_3',
+    title: '大班生活活动：整理储物柜',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/8cb6bbe4-d7f3-475d-b544-c9d6232971b6',
+  },
+  {
+    img: 'sport_1',
+    title: '小班游戏玩法：好朋友',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0026c13d-14a4-4b98-ac19-064144138025',
+  },
+  {
+    img: 'sport_2',
+    title: '中班自助玩教具：笑脸迎水花',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a1151a35-416c-4b87-9441-d60ee4d0847f',
+  },
+  {
+    img: 'sport_3',
+    title: '大班活动案例：大冲浪',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/efe61a07-7bc7-4019-92f6-c3bb4b84ecb5',
+  },
+  {
+    img: 'learn_1',
+    title: '小班科学活动：会变的月亮',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/c9a89dee-dc8c-44b0-9101-2bd58a1db0d6',
+  },
+  {
+    img: 'learn_2',
+    title: '中班语言：蚂蚁和西瓜',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fd991871-0244-49d9-97b2-be26ec525557',
+  },
+  {
+    img: 'learn_3',
+    title: '大班学习活动：天气预报',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/d3290c8c-9f7b-4567-ba75-562b6cdc51eb',
+  },
+  {
+    img: 'game_1',
+    title: '小班自制玩教具：粘来粘去',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/37771098-4c99-4c3b-a18a-dcc85064107a',
+  },
+  {
+    img: 'game_2',
+    title: '中班分享交流：小朋友，来排队',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/81c55065-fb70-47ed-b437-4f265ed81dcb',
+  },
+  {
+    img: 'game_3',
+    title: '大班游戏中的故事：变大的水渠',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/dc0d5fd1-aa91-4732-b276-77a083058901',
+  },
 ];
