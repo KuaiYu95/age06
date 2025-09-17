d1a037b655099fe0e12562508ad38d516ec72340
KuaiYu95 <ky.kyy@qq.com>
2025-09-17 22:13:54 +0800

    feat: 917 完成

---
+++ data/20250917_origin.js
@@ -0,0 +1,122 @@
+const data = [
+  {
+    "img": "banner_1",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0807/45686/index.html#/"
+  },
+  {
+    "img": "banner_2",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0731/54705/index.html#/"
+  },
+  {
+    "img": "banner_3",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/kct/02/index.html#/"
+  },
+  {
+    "img": "banner_4",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0908/57441/index.html"
+  },
+  {
+    "img": "banner_5",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/12/index.html#/"
+  },
+  {
+    "img": "banner_6",
+    "title": "9-10月课程资源征集通知",
+    "href": "https://age06.edu.sh.cn/age06web/home/content/2025/7368522496175247360.html?setOfId=6938&setOfId=6938"
+  },
+  {
+    "img": "topic_left",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/kct/01/index.html#/"
+  },
+  {
+    "img": "topic_right_1",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/296e2e42-5f53-4f25-a695-6594402ac848"
+  },
+  {
+    "img": "topic_right_2",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/05/index.html#/"
+  },
+  {
+    "img": "life_1",
+    "title": "小班生活活动：老师在一起",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fb8c233e-95f4-45ff-864e-8bcd88e25654"
+  },
+  {
+    "img": "life_2",
+    "title": "中班生活：制定游戏规则",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/f9a3cc39-1f65-41b4-a76a-b90069c9ee3b"
+  },
+  {
+    "img": "life_3",
+    "title": "大班生活活动：整理储物柜",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/8cb6bbe4-d7f3-475d-b544-c9d6232971b6"
+  },
+  {
+    "img": "sport_1",
+    "title": "小班游戏玩法：好朋友",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0026c13d-14a4-4b98-ac19-064144138025"
+  },
+  {
+    "img": "sport_2",
+    "title": "中班自助玩教具：笑脸迎水花",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a1151a35-416c-4b87-9441-d60ee4d0847f"
+  },
+  {
+    "img": "sport_3",
+    "title": "大班活动案例：大冲浪",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/efe61a07-7bc7-4019-92f6-c3bb4b84ecb5"
+  },
+  {
+    "img": "learn_1",
+    "title": "小班科学活动：会变的月亮",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/c9a89dee-dc8c-44b0-9101-2bd58a1db0d6"
+  },
+  {
+    "img": "learn_2",
+    "title": "中班语言：蚂蚁和西瓜",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fd991871-0244-49d9-97b2-be26ec525557"
+  },
+  {
+    "img": "learn_3",
+    "title": "大班学习活动：天气预报",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/d3290c8c-9f7b-4567-ba75-562b6cdc51eb"
+  },
+  {
+    "img": "game_1",
+    "title": "小班自制玩教具：粘来粘去",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/37771098-4c99-4c3b-a18a-dcc85064107a"
+  },
+  {
+    "img": "game_2",
+    "title": "中班分享交流：小朋友，来排队",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/81c55065-fb70-47ed-b437-4f265ed81dcb"
+  },
+  {
+    "img": "game_3",
+    "title": "大班游戏中的故事：变大的水渠",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/dc0d5fd1-aa91-4732-b276-77a083058901"
+  },
+  {
+    "img": "material_1",
+    "title": "主题素材包：四季之四季特征（秋）",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6b674797-e468-4015-87ed-8cf9d2350b8b"
+  },
+  {
+    "img": "material_2",
+    "title": "主题素材包：植物的生长条件",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/c0ae0fd7-5252-416e-887c-76fcb0115c87"
+  },
+  {
+    "img": "material_3",
+    "title": "主题素材包：天气现象之雨",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/3ef3f373-b997-40b1-92d2-2fab63c7e6ad"
+  }
+];
