1b84c72e7f3ed333a3bf51584bcaf102f1f811bc
KuaiYu95 <ky.kyy@qq.com>
2025-09-18 22:59:06 +0800

    feat: 20250918 第四期改版首页完成

---
+++ data/20250918_origin.js
@@ -0,0 +1,122 @@
+const data = [
+  {
+    "img": "banner_1",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0807/45686/index.html#/"
+  },
+  {
+    "img": "banner_2",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0731/54705/index.html#/"
+  },
+  {
+    "img": "banner_3",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/kct/02/index.html#/"
+  },
+  {
+    "img": "banner_4",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0908/57441/index.html"
+  },
+  {
+    "img": "banner_5",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/12/index.html#/"
+  },
+  {
+    "img": "banner_6",
+    "title": "9-10月课程资源征集通知",
+    "href": "https://age06.edu.sh.cn/age06web/home/content/2025/7368522496175247360.html?setOfId=6938&setOfId=6938"
+  },
+  {
+    "img": "topic_left",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/kct/01/index.html#/"
+  },
+  {
+    "img": "topic_right_1",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/296e2e42-5f53-4f25-a695-6594402ac848"
+  },
+  {
+    "img": "topic_right_2",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/05/index.html#/"
+  },
+  {
+    "img": "life_1",
+    "title": "小班生活活动：男女不同",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6b94c0d1-f260-47f1-b4ea-e4ccf8990f69"
+  },
+  {
+    "img": "life_2",
+    "title": "中班指导提示：国庆节",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a4afff74-467b-4c75-a267-41c2c7a01602"
+  },
+  {
+    "img": "life_3",
+    "title": "大班生活活动：礼仪",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/add6a787-8cb7-4a45-816c-49e2473b464e"
+  },
+  {
+    "img": "sport_1",
+    "title": "小班游戏玩法：疯狂的坦克",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/ab1e6de8-9ac1-40c9-9a15-b71ec82f85af"
+  },
+  {
+    "img": "sport_2",
+    "title": "中班运动：消防员训练",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/324356a1-fa48-4499-9579-b4d7b9ec40af"
+  },
+  {
+    "img": "sport_3",
+    "title": "大班活动案例：小小侦察兵",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/648cf10e-362d-4fe6-a8ba-6e5c19998822"
+  },
+  {
+    "img": "learn_1",
+    "title": "小班语言：星星警察",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/02634fc7-9eeb-4c8c-8ccf-48512e22f8ef"
+  },
+  {
+    "img": "learn_2",
+    "title": "中班自助玩教具：最佳安检员",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/52e13f18-3bf5-43e2-a0f0-ed62ca39c6b2"
+  },
+  {
+    "img": "learn_3",
+    "title": "大班谈话活动：我的名字叫“建国”",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1252b6f1-3e76-40a2-adbe-0ac3c24400f5"
+  },
+  {
+    "img": "game_1",
+    "title": "小班游戏中的故事：消防员和消防车",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6e37cff3-7ad1-41b4-8754-e99b6bc9f230"
+  },
+  {
+    "img": "game_2",
+    "title": "中班分享交流：建构—坦克车像不像",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0cc38d66-416f-4aea-ad0f-c8bbed343d53"
+  },
+  {
+    "img": "game_3",
+    "title": "大班游戏中的故事：娃娃从军记",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/c1a19236-6728-4cdc-91f9-ab90f3ab01d9"
+  },
+  {
+    "img": "material_1",
+    "title": "主题素材包：中国美景之古迹（故宫）",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1dc01d08-a3b9-424c-bd8a-9244e7b54b2e"
+  },
+  {
+    "img": "material_2",
+    "title": "主题素材包：中国美景之古迹（长城）",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/74e77f9e-58aa-45c6-97a2-9930cfc60034"
+  },
+  {
+    "img": "material_3",
+    "title": "音乐：爱我中华",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/7fd12277-d21f-4f08-947b-3a8854a6cf1e"
+  }
+];
