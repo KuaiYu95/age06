22f1f7cae4f11e2e5285d1d8084f81cbfb1d3fd3
yu.kuai <yu.kuai@chaitin.com>
2025-09-28 16:48:28 +0800

    feat: 第7期完成

---
+++ data/20250928_origin.js
@@ -0,0 +1,122 @@
+const data = [
+  {
+    "img": "banner_1",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0807/45686/index.html#/"
+  },
+  {
+    "img": "banner_2",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0731/54705/index.html#/"
+  },
+  {
+    "img": "banner_3",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0926/48600/index.html"
+  },
+  {
+    "img": "banner_4",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0908/57441/index.html"
+  },
+  {
+    "img": "banner_5",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/12/index.html#/"
+  },
+  {
+    "img": "banner_6",
+    "title": "9-10月课程资源征集通知",
+    "href": "https://age06.edu.sh.cn/age06web/home/content/2025/7368522496175247360.html?setOfId=6938&setOfId=6938"
+  },
+  {
+    "img": "topic_left",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/kct/02/index.html#/"
+  },
+  {
+    "img": "topic_right_1",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b2c0a175-999a-40ad-8198-ee67521bb615"
+  },
+  {
+    "img": "topic_right_2",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/39505059-c555-46fb-81a1-e9e443dc756e"
+  },
+  {
+    "img": "life_1",
+    "title": "小班生活活动：多喝水身体好",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/915cd504-6c1b-4530-9ce3-93a924eea6b5"
+  },
+  {
+    "img": "life_2",
+    "title": "中班生活活动：出行方式",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b94b6ca8-f2f5-4033-93cf-ee29ea3ecfad"
+  },
+  {
+    "img": "life_3",
+    "title": "大班生活活动：春夏秋冬",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e834f4ae-38da-4997-a7ef-7f88fe0978bf"
+  },
+  {
+    "img": "sport_1",
+    "title": "小班自制运动器械：推小球",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/790eeede-13ee-47fd-878a-5c704201920a"
+  },
+  {
+    "img": "sport_2",
+    "title": "中班活动案例：运送粮食",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2057fa1a-35b2-49ca-8557-89832ec85631"
+  },
+  {
+    "img": "sport_3",
+    "title": "大班器械体操：响铃操",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e88a8d56-5981-4614-a624-2298bd54d226"
+  },
+  {
+    "img": "learn_1",
+    "title": "小班语言：小圆球旅行记",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6eb3b905-adb8-4439-b5c0-88a66533c63f"
+  },
+  {
+    "img": "learn_2",
+    "title": "中班科学活动：走进万亩良田",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b7e05d94-c0c2-4d44-ac04-2e6358c2efc6"
+  },
+  {
+    "img": "learn_3",
+    "title": "大班科学活动：计划一次野营",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b2a7bec1-fa8f-4b77-af2f-830a918c7660"
+  },
+  {
+    "img": "game_1",
+    "title": "小班分享交流：坐车可以去哪里？",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1aed0576-c503-4238-867b-60b196a53c88"
+  },
+  {
+    "img": "game_2",
+    "title": "中班分享交流：民间游戏 拾果果",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/c01d48ed-2b0c-4162-af5a-a06b275afb16"
+  },
+  {
+    "img": "game_3",
+    "title": "大班游戏中的故事：糖果店里包糖果",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e9c08619-ec5c-45d1-a4ca-c2bc35fed618"
+  },
+  {
+    "img": "material_1",
+    "title": "主题素材包：各种各样的植物之银杏",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/3987f53f-8abd-4d59-b8a2-3d92ad74e389"
+  },
+  {
+    "img": "material_2",
+    "title": "树叶的朋友-成形动画",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/c52057a7-ff18-4881-83dc-cb73c14c3e06"
+  },
+  {
+    "img": "material_3",
+    "title": "树叶的朋友-剪切拼贴",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/3fa80f7b-071b-4c4b-9dbd-d78962c6e4d5"
+  }
+];
--- data/new_data.js
+++ data/new_data.js
@@ -1,7 +1,82 @@
 const data = [
+  // 课程资源 - 生活板块 1/2/3
   {
-    img: 'topic_left',
-    title: '',
-    href: 'https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/kct/02/index.html#/',
+    img: 'life_1',
+    title: '小班生活：紧急电话牢牢记',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/4c4ca9a3-e2c1-45e3-87d8-af2ca74dc65b',
+  },
+  {
+    img: 'life_2',
+    title: '中班生活：马路安全我知道',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0f83f10e-3900-4d23-8f25-df0d752559c8',
+  },
+  {
+    img: 'life_3',
+    title: '大班生活：乘梯安全我知道',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0bc4f9df-c919-46ee-a24d-38800acde71c',
+  },
+  // 课程资源 - 运动板块 1/2/3
+  {
+    img: 'sport_1',
+    title: '小班区域性运动：赛车总动员',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6ff7ce2f-c3ef-452c-bef0-0006f20ae48f',
+  },
+  {
+    img: 'sport_2',
+    title: '中班活动案例：汽车快快跑',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6e3f8bbc-e190-4743-a9cd-eabd2a3272ae',
+  },
+  {
+    img: 'sport_3',
+    title: '大班活动案例：逛上海',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/dad81b9c-3d5e-41a1-b7c2-5eb3f50685b9',
+  },
+  // 课程资源 - 学习板块 1/2/3
+  {
+    img: 'learn_1',
+    title: '小班科学活动：动物照相馆',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2376c965-196b-4a4c-ac4e-8e0d14b245cf',
+  },
+  {
+    img: 'learn_2',
+    title: '中班数学：小动物住酒店',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/dee02f60-193b-40eb-8bc6-66f8e7fbbe23',
+  },
+  {
+    img: 'learn_3',
+    title: '大班艺术：千里江山图',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/8c928f93-216b-422b-afff-0f3463f01629',
+  },
+  // 课程资源 - 游戏板块 1/2/3
+  {
+    img: 'game_1',
+    title: '小班游戏中的故事：热闹的美食街',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/57abd10f-7d15-4c6c-a426-eb9d86d7adc4',
+  },
+  {
+    img: 'game_2',
+    title: '中班介入回应：好大的游轮',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/ff71c8fb-b71a-4699-9e6a-90ce6360a250',
+  },
+  {
+    img: 'game_3',
+    title: '大班游戏中的故事：航海地图',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0506f9ec-b83e-4188-a5db-58e3cd8c58fd',
+  },
+  // 素材资源 1/2/3
+  {
+    img: 'material_1',
+    title: '主题素材包：各种各样的交通工具之汽车',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fa3a47d2-5411-40a6-81a9-486144a6e68e',
+  },
+  {
+    img: 'material_2',
+    title: '主题素材包：各种各样的交通工具之船',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a4880503-8dbd-4350-af31-76c01ee2a382',
+  },
+  {
+    img: 'material_3',
+    title: '主题素材包：各种各样的交通工具之飞机',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/79a67c86-f77a-483f-bf18-b4a67ae3cfc1',
   },
 ];
