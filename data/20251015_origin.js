41511333660f7c89daceb605a70a2585e2ecf1b6
KuaiYu95 <ky.kyy@qq.com>
2025-10-15 23:01:13 +0800

    feat: 第10期素材更新

---
+++ data/20251015_origin.js
@@ -0,0 +1,122 @@
+const data = [
+  {
+    "img": "banner_1",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0807/45686/index.html#/"
+  },
+  {
+    "img": "banner_2",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0731/54705/index.html#/"
+  },
+  {
+    "img": "banner_3",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0926/48600/index.html"
+  },
+  {
+    "img": "banner_4",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0908/57441/index.html"
+  },
+  {
+    "img": "banner_5",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/12/index.html#/"
+  },
+  {
+    "img": "banner_6",
+    "title": "9-10月课程资源征集通知",
+    "href": "https://age06.edu.sh.cn/age06web/home/content/2025/7368522496175247360.html?setOfId=6938&setOfId=6938"
+  },
+  {
+    "img": "topic_left",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/kct/02/index.html#/"
+  },
+  {
+    "img": "topic_right_1",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b2c0a175-999a-40ad-8198-ee67521bb615"
+  },
+  {
+    "img": "topic_right_2",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/39505059-c555-46fb-81a1-e9e443dc756e"
+  },
+  {
+    "img": "life_1",
+    "title": "小班指导提示：让午睡也有序起来",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/f6b91778-375f-4953-825f-bf352f457da0"
+  },
+  {
+    "img": "life_2",
+    "title": "中班生活活动：节约用纸",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0812564f-51c7-4ebc-90ac-2b90113d4000"
+  },
+  {
+    "img": "life_3",
+    "title": "大班生活活动：认识细菌",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/c9c103a8-a807-4595-a908-4f5c1d6308c9"
+  },
+  {
+    "img": "sport_1",
+    "title": "中班运动活动：蹦蹦跳跳",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5feafc02-9b3e-4d67-ae40-ef44629185ea"
+  },
+  {
+    "img": "sport_2",
+    "title": "大班运动活动：好玩的长布条",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/bbf5329c-f02f-46ce-91f6-0792ddbfd349"
+  },
+  {
+    "img": "sport_3",
+    "title": "小班游戏玩法：勤劳的小牛",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/ea2f5d16-532f-4b70-a318-fc39cb34e149"
+  },
+  {
+    "img": "learn_1",
+    "title": "小班艺术：别吵，小宝宝睡了",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9c667984-53c5-4a73-b386-04a455aee675"
+  },
+  {
+    "img": "learn_2",
+    "title": "中班科学：有趣的螃蟹",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2c71c17a-a92e-4453-95eb-4becdce489c5"
+  },
+  {
+    "img": "learn_3",
+    "title": "大班美术活动：橙子变变变",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0235bcb3-c0dc-4a2e-8420-a736fba8c12a"
+  },
+  {
+    "img": "game_1",
+    "title": "小班环境材料：小小水果店",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/ba422ed7-bc76-481e-983d-19f696a36ea7"
+  },
+  {
+    "img": "game_2",
+    "title": "中班分享交流：了不起的交警",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a49ebc5a-a249-4a52-b604-526e0e66ee5d"
+  },
+  {
+    "img": "game_3",
+    "title": "大班自制玩教具：瓶盖趣多多",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5d710e09-512e-4826-b757-86728133c668"
+  },
+  {
+    "img": "material_1",
+    "title": "主题素材包：动物间的竞争关系",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/72d47c8c-2b57-4cba-b7a4-cdf01ac6388f"
+  },
+  {
+    "img": "material_2",
+    "title": "小剧场-环境材料说明压缩包",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/bd9fe8f5-1672-4d4c-bf76-a33d793a9bff"
+  },
+  {
+    "img": "material_3",
+    "title": "主题素材包：各种各样的动物之乌龟",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fa5fb912-f5be-412c-9e2f-dd8756e23b9d"
+  }
+];
--- data/new_data.js
+++ data/new_data.js
@@ -1,82 +1,97 @@
 const data = [
+  {
+    "img": "topic_left",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0731/54705/index.html#/"
+  },
+  {
+    "img": "topic_right_1",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/dfcea8d3-3a05-4f85-9709-58316b241802"
+  },
+  {
+    "img": "topic_right_2",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9839cc80-0f63-4d3f-bf65-4d7b3e0a71bc"
+  },
   // 课程资源 - 生活板块 1/2/3
   {
     img: 'life_1',
-    title: '小班指导提示：让午睡也有序起来',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/f6b91778-375f-4953-825f-bf352f457da0',
+    title: '小班生活活动：节约粮食',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/910c0f17-9247-4bab-9da3-8ea568e8659c'
   },
   {
     img: 'life_2',
-    title: '中班生活活动：节约用纸',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0812564f-51c7-4ebc-90ac-2b90113d4000',
+    title: '中班生活活动：认识餐具',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/858cd9e4-7aaa-4d7e-a259-df082755f720'
   },
   {
     img: 'life_3',
-    title: '大班生活活动：认识细菌',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/c9c103a8-a807-4595-a908-4f5c1d6308c9',
+    title: '大班案例分享：营养我知道',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6be72976-dfb3-4e7e-82d3-9b0e41de73ec'
   },
   // 课程资源 - 运动板块 1/2/3
   {
     img: 'sport_1',
-    title: '中班运动活动：蹦蹦跳跳',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5feafc02-9b3e-4d67-ae40-ef44629185ea',
+    title: '小班活动案例：给小动物送食物',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/3b3c4672-ec09-4c85-91b4-0b50bce4cb3c'
   },
   {
     img: 'sport_2',
-    title: '大班运动活动：好玩的长布条',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/bbf5329c-f02f-46ce-91f6-0792ddbfd349',
+    title: '中班集体运动游戏：炒豆豆',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/51186a20-5e31-44f4-ac50-e33c5d63a7e5'
   },
   {
     img: 'sport_3',
-    title: '小班游戏玩法：勤劳的小牛',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/ea2f5d16-532f-4b70-a318-fc39cb34e149',
+    title: '大班游戏玩法：送粮',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/d403e91d-18fb-485c-a80b-5bd24fac25de'
   },
   // 课程资源 - 学习板块 1/2/3
   {
     img: 'learn_1',
-    title: '小班艺术：别吵，小宝宝睡了',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9c667984-53c5-4a73-b386-04a455aee675',
+    title: '小班健康：爱惜粮食',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fb09a7f4-cc08-42cd-bfaa-f60587a93d79'
   },
   {
     img: 'learn_2',
-    title: '中班科学：有趣的螃蟹',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2c71c17a-a92e-4453-95eb-4becdce489c5',
+    title: '中班健康：不挑食，营养均衡',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/bf571170-13e5-48f4-888f-65db7fd7f43e'
   },
   {
     img: 'learn_3',
-    title: '大班美术活动：橙子变变变',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0235bcb3-c0dc-4a2e-8420-a736fba8c12a',
+    title: '全年龄段科学活动：开心农场',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1e66b2aa-d1c0-4863-867d-102108bae90c'
   },
   // 课程资源 - 游戏板块 1/2/3
   {
     img: 'game_1',
-    title: '小班环境材料：小小水果店',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/ba422ed7-bc76-481e-983d-19f696a36ea7',
+    title: '小班分享交流：贝贝餐厅',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/4d599e3e-ba66-4cae-8a52-dd5aa1c36116'
   },
   {
     img: 'game_2',
-    title: '中班分享交流：了不起的交警',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a49ebc5a-a249-4a52-b604-526e0e66ee5d',
+    title: '中班介入回应：新式蛋糕诞生了',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b1b09373-9e5a-4348-b961-c89f9bf50488'
   },
   {
     img: 'game_3',
-    title: '大班自制玩教具：瓶盖趣多多',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5d710e09-512e-4826-b757-86728133c668',
+    title: '大班自制玩教具：大蒜哥和玉米妹',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/87f04c96-441a-4cc7-9f79-40cb6be84f7d'
   },
   // 素材资源 1/2/3
   {
-    "img": "material_1",
-    "title": "主题素材包：动物间的竞争关系",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/72d47c8c-2b57-4cba-b7a4-cdf01ac6388f",
+    img: "material_1",
+    title: "主题素材包：对健康的作用之护眼作用",
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/24eabfc5-75a2-451c-b38b-fd091d2211a4'
   },
   {
-    "img": "material_2",
-    "title": "小剧场-环境材料说明压缩包",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/bd9fe8f5-1672-4d4c-bf76-a33d793a9bff"
+    img: "material_2",
+    title: "主题素材包：人类的生存离不开动物",
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/843dc1ff-c82d-4714-9791-299d828b2806'
   },
   {
-    "img": "material_3",
-    "title": "主题素材包：各种各样的动物之乌龟",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fa5fb912-f5be-412c-9e2f-dd8756e23b9d"
+    img: "material_3",
+    title: "口腔健康“吃”出来",
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/7c0a2304-daba-4e26-87c6-27831f97e4bd'
   }
 ];
