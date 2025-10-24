52f69b4dd00b2739fe8495d727f188f7d950a0dc
yu.kuai <yu.kuai@chaitin.com>
2025-10-24 12:35:37 +0800

    12期

---
+++ data/20251024_origin.js
@@ -0,0 +1,122 @@
+const data = [
+  {
+    "img": "banner_1",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0807/45686/index.html#/"
+  },
+  {
+    "img": "banner_2",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/1021/14666/index.html"
+  },
+  {
+    "img": "banner_3",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0926/48600/index.html"
+  },
+  {
+    "img": "banner_4",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0908/57441/index.html"
+  },
+  {
+    "img": "banner_5",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/12/index.html#/"
+  },
+  {
+    "img": "banner_6",
+    "title": "9-10月课程资源征集通知",
+    "href": "https://age06.edu.sh.cn/age06web/home/content/2025/7368522496175247360.html?setOfId=6938&setOfId=6938"
+  },
+  {
+    "img": "topic_left",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0731/54705/index.html#/"
+  },
+  {
+    "img": "topic_right_1",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/dfcea8d3-3a05-4f85-9709-58316b241802"
+  },
+  {
+    "img": "topic_right_2",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9839cc80-0f63-4d3f-bf65-4d7b3e0a71bc"
+  },
+  {
+    "img": "life_1",
+    "title": "小班生活：指甲长了怎么办",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0ae059e4-636c-48df-8927-66e2312a717c"
+  },
+  {
+    "img": "life_2",
+    "title": "中班生活活动：小手真能干",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/678a3e9f-3eb9-4845-aef1-c79b2e4fbad5"
+  },
+  {
+    "img": "life_3",
+    "title": "大班指导提示：我的柜子我来理",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0bfe6916-b193-412a-931f-dea1c2770ec7"
+  },
+  {
+    "img": "sport_1",
+    "title": "小班亲子：送雨伞",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/066c42c4-17e8-4383-97a3-182cda90bf12"
+  },
+  {
+    "img": "sport_2",
+    "title": "中班自制玩教具：有趣的罐子",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/d5e12369-05b3-4db7-81c9-f4f61bc8d7bc"
+  },
+  {
+    "img": "sport_3",
+    "title": "大班游戏玩法：龙虎斗",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/4d7c3a8c-a874-4c2b-9b01-5a911a9be8a2"
+  },
+  {
+    "img": "learn_1",
+    "title": "小班语言活动：小红鱼找朋友",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/62c7ebdc-22e0-401c-a40a-6c73685c0a00"
+  },
+  {
+    "img": "learn_2",
+    "title": "中班数学：熊先生的糖果店",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6556dca4-8685-4804-aa1d-94b542ad4e01"
+  },
+  {
+    "img": "learn_3",
+    "title": "大班音乐活动：牛宝宝音乐盒",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/eaca24e3-ceb3-4291-9ba5-2e846fe9f79a"
+  },
+  {
+    "img": "game_1",
+    "title": "小班游戏中的故事：“你们怎么不说话呀”",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1d9b8b9c-19e2-4478-8204-078c7f427389"
+  },
+  {
+    "img": "game_2",
+    "title": "中班分享交流：把积木“缝”起来！",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0ecd0eda-e566-42e5-a0f3-5ad52e30aa7e"
+  },
+  {
+    "img": "game_3",
+    "title": "大班自制玩教具：多变的二十八",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/cc902476-62e4-4710-9e96-511f191b0bac"
+  },
+  {
+    "img": "material_1",
+    "title": "主题素材包：天气现象之雨",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/3ef3f373-b997-40b1-92d2-2fab63c7e6ad"
+  },
+  {
+    "img": "material_2",
+    "title": "压缩包：绕过障碍物",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/3454784c-e1b5-48ff-8d3b-55d5b43a0653"
+  },
+  {
+    "img": "material_3",
+    "title": "文稿课件：《黄浦江上四座桥》",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/ce4a3456-4dec-46fb-8d46-60063ee304c1"
+  }
+];
