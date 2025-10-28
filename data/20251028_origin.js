09ff5b7b10d6d93eb3f7ab346c0402e6c8cedd74
KuaiYu95 <ky.kyy@qq.com>
2025-10-28 22:50:07 +0800

    13期完成

---
+++ data/20251028_origin.js
@@ -0,0 +1,122 @@
+const data = [
+  {
+    "img": "banner_1",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0807/45686/index.html#/"
+  },
+  {
+    "img": "banner_2",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/1021/14666/index.html"
+  },
+  {
+    "img": "banner_3",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0926/48600/index.html"
+  },
+  {
+    "img": "banner_4",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0908/57441/index.html"
+  },
+  {
+    "img": "banner_5",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/12/index.html#/"
+  },
+  {
+    "img": "banner_6",
+    "title": "9-10月课程资源征集通知",
+    "href": "https://age06.edu.sh.cn/age06web/home/content/2025/7368522496175247360.html?setOfId=6938&setOfId=6938"
+  },
+  {
+    "img": "topic_left",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0731/54705/index.html#/"
+  },
+  {
+    "img": "topic_right_1",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/db1acf95-8d1d-418e-a412-e2a75a4cd3e6"
+  },
+  {
+    "img": "topic_right_2",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/24bb9829-d1ae-4201-ba8c-2e9561534007"
+  },
+  {
+    "img": "life_1",
+    "title": "小班生活活动：我不怕打针",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/dae00e7b-9005-4684-9d23-74dac6879239"
+  },
+  {
+    "img": "life_2",
+    "title": "中班案例分享：我会打喷嚏",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/161c85f9-5e7d-4a34-84ac-c76d7ad89321"
+  },
+  {
+    "img": "life_3",
+    "title": "大班案例分享：幼儿鼻出血后",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1c4a00bc-9ddb-4cee-945b-095badb4a01f"
+  },
+  {
+    "img": "sport_1",
+    "title": "大班教学案例：穿越大直径的圈",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/3fa66d81-d97e-4472-a606-93f234348043"
+  },
+  {
+    "img": "sport_2",
+    "title": "小班亲子：逮住老鼠",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/f9626b47-013e-459b-8fb0-ceea4db225f6"
+  },
+  {
+    "img": "sport_3",
+    "title": "中班游戏玩法：看信号跳圈",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2246f11e-0c22-43d1-b994-070bc8ab71c1"
+  },
+  {
+    "img": "learn_1",
+    "title": "小班科学活动：迪迪医生",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/aae4f4a7-c1ca-4940-aea5-5a59b4a1c337"
+  },
+  {
+    "img": "learn_2",
+    "title": "中班美术：菌菌世界",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/10eb5bd6-31a7-4c49-a514-de726aa1136e"
+  },
+  {
+    "img": "learn_3",
+    "title": "大班艺术：大树的医生",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9a9fda14-9b1b-475e-94f2-f2ffbce7a107"
+  },
+  {
+    "img": "game_1",
+    "title": "小班游戏中的故事：娃娃生病了”",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/c707b486-a29d-4bfd-b7c7-642d54f57547"
+  },
+  {
+    "img": "game_2",
+    "title": "中班分享交流：许多小医生",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/47defdf8-d951-4cd2-884a-e6d2707be182"
+  },
+  {
+    "img": "game_3",
+    "title": "大班游戏中的故事：医院能不能量身高",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/00eafe62-5f49-4c53-9ee8-1da34bb2006d"
+  },
+  {
+    "img": "material_1",
+    "title": "活动室布置的安全规范",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5db168e3-e865-413b-a2a2-c9407470a8b1"
+  },
+  {
+    "img": "material_2",
+    "title": "主题素材包：动物的本领之会造家动物",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e7e195ca-f25e-4f10-a6e7-0cc82a750428"
+  },
+  {
+    "img": "material_3",
+    "title": "主题素材包：植物对环境的作用之防风固沙",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/770eeec9-0efe-4686-b98d-364c71943217"
+  }
+];
--- data/new_data.js
+++ data/new_data.js
@@ -1,94 +1,82 @@
 const data = [
-  // 主题图 - 右上
-  {
-    img: 'topic_right_1',
-    title: '',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/db1acf95-8d1d-418e-a412-e2a75a4cd3e6',
-  },
-  // 主题图 - 右下
-  {
-    img: 'topic_right_2',
-    title: '',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/24bb9829-d1ae-4201-ba8c-2e9561534007',
-  },
   // 课程资源 - 生活板块 1/2/3
   {
     img: 'life_1',
-    title: '小班生活活动：我不怕打针',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/dae00e7b-9005-4684-9d23-74dac6879239',
+    title: '小班生活：我会自己穿衣服',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1b9d7eb3-cea2-4fb1-8236-3eb7f4308220',
   },
   {
     img: 'life_2',
-    title: '中班案例分享：我会打喷嚏',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/161c85f9-5e7d-4a34-84ac-c76d7ad89321',
+    title: '中班指导提示：包好小肚子',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/92a13658-9403-4d61-a2bd-55212402acb1',
   },
   {
     img: 'life_3',
-    title: '大班案例分享：幼儿鼻出血后',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1c4a00bc-9ddb-4cee-945b-095badb4a01f',
+    title: '大班环境创设：便后，穿戴整齐',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2881e79f-d8b1-48d2-869a-436819c14e95',
   },
   // 课程资源 - 运动板块 1/2/3
   {
     img: 'sport_1',
-    title: '大班教学案例：穿越大直径的圈',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/3fa66d81-d97e-4472-a606-93f234348043',
+    title: '小班游戏玩法：好朋友',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0026c13d-14a4-4b98-ac19-064144138025',
   },
   {
     img: 'sport_2',
-    title: '小班亲子：逮住老鼠',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/f9626b47-013e-459b-8fb0-ceea4db225f6',
+    title: '中班集体运动游戏：神秘好朋友',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/aa1470e3-83bc-46e5-bfc4-3505afe9fe4d',
   },
   {
     img: 'sport_3',
-    title: '中班游戏玩法：看信号跳圈',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2246f11e-0c22-43d1-b994-070bc8ab71c1',
+    title: '大班游戏玩法：找朋友',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a3645b53-6262-4796-9fb5-e29d78062b76',
   },
   // 课程资源 - 学习板块 1/2/3
   {
     img: 'learn_1',
-    title: '小班科学活动：迪迪医生',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/aae4f4a7-c1ca-4940-aea5-5a59b4a1c337',
+    title: '小班科学：水宝宝找朋友',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1498af74-4627-4723-90ba-d377ebe6569b',
   },
   {
     img: 'learn_2',
-    title: '中班美术：菌菌世界',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/10eb5bd6-31a7-4c49-a514-de726aa1136e',
+    title: '中班社会：学会原谅',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/f5c30c87-7cd8-4203-9ca4-0ee4b295cc78',
   },
   {
     img: 'learn_3',
-    title: '大班艺术：大树的医生',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9a9fda14-9b1b-475e-94f2-f2ffbce7a107',
+    title: '大班环境创设：我和别人不一样',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/25e9986f-7686-4c36-ba43-127e59375c77',
   },
   // 课程资源 - 游戏板块 1/2/3
   {
     img: 'game_1',
-    title: '小班游戏中的故事：娃娃生病了”',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/c707b486-a29d-4bfd-b7c7-642d54f57547',
+    title: '小班游戏活动：娃娃家',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/22539136-1f0a-48aa-9db1-3e160d80e191',
   },
   {
     img: 'game_2',
-    title: '中班分享交流：许多小医生',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/47defdf8-d951-4cd2-884a-e6d2707be182',
+    title: '中班游戏中的故事：商量是个好办法',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/503afe4f-c7fc-4c70-ac83-2bc0d0795170',
   },
   {
     img: 'game_3',
-    title: '大班游戏中的故事：医院能不能量身高',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/00eafe62-5f49-4c53-9ee8-1da34bb2006d',
+    title: '大班分享交流：我们都不想扮演“坏狐狸”',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/3db32f7a-fd18-4001-887f-4c8b6c18309f',
   },
   // 素材资源 1/2/3
   {
     img: 'material_1',
-    title: '活动室布置的安全规范',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5db168e3-e865-413b-a2a2-c9407470a8b1',
+    title: '课件文档：有用的黄豆',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5bf9592d-e81b-46fe-b533-6373b09bd416',
   },
   {
     img: 'material_2',
-    title: '主题素材包：动物的本领之会造家动物',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e7e195ca-f25e-4f10-a6e7-0cc82a750428',
+    title: '玩法文档：老鹰抓小鸡',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/d11de5da-dea8-43e3-b89a-20e3e6eb406f',
   },
   {
     img: 'material_3',
-    title: '主题素材包：植物对环境的作用之防风固沙',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/770eeec9-0efe-4686-b98d-364c71943217',
+    title: '游戏压缩包：躲猫猫',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/4f95af82-496b-4dda-b8f3-0a256629b049',
   },
 ];
