c86eaea63112c3ddb4d94e773152719dc10a2cdc
yu.kuai <yu.kuai@chaitin.com>
2025-11-06 14:25:22 +0800

    11-2 期完成

---
+++ data/20251106_origin.js
@@ -0,0 +1,122 @@
+const data = [
+  {
+    "img": "banner_1",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0807/45686/index.html#/"
+  },
+  {
+    "img": "banner_2",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/1021/14666/index.html"
+  },
+  {
+    "img": "banner_3",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0926/48600/index.html"
+  },
+  {
+    "img": "banner_4",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0908/57441/index.html"
+  },
+  {
+    "img": "banner_5",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/12/index.html#/"
+  },
+  {
+    "img": "banner_6",
+    "title": "9-10月课程资源征集通知",
+    "href": "https://age06.edu.sh.cn/age06web/home/content/2025/7368522496175247360.html?setOfId=6938&setOfId=6938"
+  },
+  {
+    "img": "topic_left",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0731/54705/index.html#/"
+  },
+  {
+    "img": "topic_right_1",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/864632a0-98d9-4467-a31d-e03019b00a5a"
+  },
+  {
+    "img": "topic_right_2",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5db168e3-e865-413b-a2a2-c9407470a8b1"
+  },
+  {
+    "img": "life_1",
+    "title": "中班生活活动：保护皮肤",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2237d12a-d359-43d6-a130-7d25beb0ea2f"
+  },
+  {
+    "img": "life_2",
+    "title": "大班生活活动：春夏秋冬",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e834f4ae-38da-4997-a7ef-7f88fe0978bf"
+  },
+  {
+    "img": "life_3",
+    "title": "小班环境创设：抹香香",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/403a732f-370a-4021-b4ca-06b969ba2350"
+  },
+  {
+    "img": "sport_1",
+    "title": "小班亲子：猜动物",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5dfc7f7e-4629-4473-97b7-ad154fb0583c"
+  },
+  {
+    "img": "sport_2",
+    "title": "中班教学案例：小马学本领",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/45068cf8-6951-4a1f-ae0b-32d91c3f3c88"
+  },
+  {
+    "img": "sport_3",
+    "title": "大班游戏玩法：麻袋跑跑",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/643dc26b-1474-41b4-8514-86575e2a9fa9"
+  },
+  {
+    "img": "learn_1",
+    "title": "小班科学：买冬衣 ",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/8a8c68cb-7a92-400c-8084-97775fd533fa"
+  },
+  {
+    "img": "learn_2",
+    "title": "中班艺术：雪地里的小画家",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fc9cb390-c7e7-4b49-9ae7-11ff3830b333"
+  },
+  {
+    "img": "learn_3",
+    "title": "大班健康：冬天爱运动",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b42ab05d-b726-42de-b7d3-0b992a4d6d44"
+  },
+  {
+    "img": "game_1",
+    "title": "小班游戏中的故事：忙碌的爷爷",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/d91f4b88-e952-49c6-b97c-77348230dc8f"
+  },
+  {
+    "img": "game_2",
+    "title": "中班环境材料：插塑类材料—雪花片",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/788a8ec9-9a7b-4479-8fb8-3b46fd278127"
+  },
+  {
+    "img": "game_3",
+    "title": "大班介入回应：幼儿户外积木游戏",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/d4dddd4a-74ad-4ff4-acc8-916b135232e5"
+  },
+  {
+    "img": "material_1",
+    "title": "主题素材包：四季之四季特征（冬）",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/43edbba8-ef19-49eb-a022-9187cad3e2bb"
+  },
+  {
+    "img": "material_2",
+    "title": "主题素材包：天气现象之雪是怎么形成的",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0d18ebbc-3079-497e-9407-9472380ae230"
+  },
+  {
+    "img": "material_3",
+    "title": "音乐素材：“买菜”歌曲",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/92db0541-6e3a-4d46-a9e2-ae0e7b7b91f1"
+  }
+];
--- data/new_data.js
+++ data/new_data.js
@@ -1,94 +1,82 @@
 const data = [
-  // 主题图 - 右上
-  {
-    img: 'topic_right_1',
-    title: '',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/864632a0-98d9-4467-a31d-e03019b00a5a',
-  },
-  // 主题图 - 右下
-  {
-    img: 'topic_right_2',
-    title: '',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5db168e3-e865-413b-a2a2-c9407470a8b1',
-  },
   // 课程资源 - 生活板块 1/2/3
   {
     img: 'life_1',
-    title: '中班生活活动：保护皮肤',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2237d12a-d359-43d6-a130-7d25beb0ea2f',
+    title: '小班生活：紧急电话牢牢记',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/4c4ca9a3-e2c1-45e3-87d8-af2ca74dc65b',
   },
   {
     img: 'life_2',
-    title: '大班生活活动：春夏秋冬',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e834f4ae-38da-4997-a7ef-7f88fe0978bf',
+    title: '大班生活活动：着火了怎么办',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/972d9cff-a455-4683-b227-8dec24c7eb84',
   },
   {
     img: 'life_3',
-    title: '小班环境创设：抹香香',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/403a732f-370a-4021-b4ca-06b969ba2350',
+    title: '中班环境创设：安全角',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b6824816-4f91-4c69-94a7-9ff1e4b2905a',
   },
   // 课程资源 - 运动板块 1/2/3
   {
     img: 'sport_1',
-    title: '小班亲子：猜动物',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5dfc7f7e-4629-4473-97b7-ad154fb0583c',
+    title: '小班活动案例：托马斯汽车城',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/81a06d9d-f924-4588-bfab-6b2ac93362ce',
   },
   {
     img: 'sport_2',
-    title: '中班教学案例：小马学本领',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/45068cf8-6951-4a1f-ae0b-32d91c3f3c88',
+    title: '中班运动：消防员训练',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/324356a1-fa48-4499-9579-b4d7b9ec40af',
   },
   {
     img: 'sport_3',
-    title: '大班游戏玩法：麻袋跑跑',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/643dc26b-1474-41b4-8514-86575e2a9fa9',
+    title: '大班教学案例：消防训练营',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e237ab5c-dae8-48fc-80ce-1aedd71ae799',
   },
   // 课程资源 - 学习板块 1/2/3
   {
     img: 'learn_1',
-    title: '小班科学：买冬衣 ',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/8a8c68cb-7a92-400c-8084-97775fd533fa',
+    title: '小班科学：快快赶往“救援现场”',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/642d0f88-e6e1-4ee6-9183-ffa36c9bb766',
   },
   {
     img: 'learn_2',
-    title: '中班艺术：雪地里的小画家',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fc9cb390-c7e7-4b49-9ae7-11ff3830b333',
+    title: '中班社会：参观消防局',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b0a767ef-6f31-4978-9979-9427681f7a78',
   },
   {
     img: 'learn_3',
-    title: '大班健康：冬天爱运动',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b42ab05d-b726-42de-b7d3-0b992a4d6d44',
+    title: '大班音乐活动：造高楼',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/c3ba0ac9-6ce0-40e4-901c-033c6030c345',
   },
   // 课程资源 - 游戏板块 1/2/3
   {
     img: 'game_1',
-    title: '小班游戏中的故事：忙碌的爷爷',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/d91f4b88-e952-49c6-b97c-77348230dc8f',
+    title: '小班游戏中的故事：消防员和消防车',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/6e37cff3-7ad1-41b4-8754-e99b6bc9f230',
   },
   {
     img: 'game_2',
-    title: '中班环境材料：插塑类材料—雪花片',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/788a8ec9-9a7b-4479-8fb8-3b46fd278127',
+    title: '中班游戏中的故事：消防员墨墨',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2b87495c-6406-4962-b9f6-c11c52f10753',
   },
   {
     img: 'game_3',
-    title: '大班介入回应：幼儿户外积木游戏',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/d4dddd4a-74ad-4ff4-acc8-916b135232e5',
+    title: '大班自制玩教具：拯救失火大楼',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e02887df-a827-4d06-9c82-fdf0d2ca0479',
   },
   // 素材资源 1/2/3
   {
     img: 'material_1',
-    title: '主题素材包：四季之四季特征（冬）',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/43edbba8-ef19-49eb-a022-9187cad3e2bb',
+    title: '课件素材：了不起的消防员叔叔',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/d10f3e5a-451e-4c01-ba1e-8af16bc2ec39',
   },
   {
     img: 'material_2',
-    title: '主题素材包：天气现象之雪是怎么形成的',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0d18ebbc-3079-497e-9407-9472380ae230',
+    title: '音乐素材：建筑之歌',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/aca3b0c8-9f46-4f1b-ba56-8e381935a2b4',
   },
   {
     img: 'material_3',
-    title: '音乐素材：“买菜”歌曲',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/92db0541-6e3a-4d46-a9e2-ae0e7b7b91f1',
+    title: '主题素材包：动物的本领之会游泳的动物',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/cf16bb2a-cb7a-4f15-acc7-4101f91920fe',
   },
 ];
