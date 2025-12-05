866aaba5987277f6411f3d1ae5f3b5d168362da5
KuaiYu95 <ky.kyy@qq.com>
2025-12-05 13:27:11 +0800

    11-8

---
+++ data/20251205_origin.js
@@ -0,0 +1,122 @@
+const data = [
+  {
+    "img": "banner_1",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0807/45686/index.html#/"
+  },
+  {
+    "img": "banner_2",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/1021/14666/index.html"
+  },
+  {
+    "img": "banner_3",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0926/48600/index.html"
+  },
+  {
+    "img": "banner_4",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0908/57441/index.html"
+  },
+  {
+    "img": "banner_5",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2024/kct/12/index.html#/"
+  },
+  {
+    "img": "banner_6",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/content/2025/7392038699954868224.html?setOfId=6938"
+  },
+  {
+    "img": "topic_left",
+    "title": "",
+    "href": "https://age06.edu.sh.cn/age06web/home/06zhuanti/2025/0731/54705/index.html#/"
+  },
+  {
+    "img": "topic_right_1",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1e1292d1-cacd-4d06-97bc-d3899ea81189"
+  },
+  {
+    "img": "topic_right_2",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fa4f3eeb-37d3-42cc-b0a4-c59e64f5c5e7"
+  },
+  {
+    "img": "life_1",
+    "title": "小班生活活动：老师在一起",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fb8c233e-95f4-45ff-864e-8bcd88e25654"
+  },
+  {
+    "img": "life_2",
+    "title": "中班生活：马路安全我知道",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0f83f10e-3900-4d23-8f25-df0d752559c8"
+  },
+  {
+    "img": "life_3",
+    "title": "大班环境创设：天天来园不迟到——早班车",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9a69256f-461c-49f1-958f-8be2c4bf8390"
+  },
+  {
+    "img": "sport_1",
+    "title": "小班远足：会唱歌的车",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/cf2d5914-25ad-43ed-879a-ce36295d7a09"
+  },
+  {
+    "img": "sport_2",
+    "title": "中班远足：我在马路边",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/8a1fe2d8-5703-44ae-aaca-5c9a74d2e7f2"
+  },
+  {
+    "img": "sport_3",
+    "title": "大班活动案例：林中小路",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5c918ea0-9326-4601-8878-28e83783a5f8"
+  },
+  {
+    "img": "learn_1",
+    "title": "小班科学：什么车子开来了",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a5e1f00c-0a6c-4f1c-bfd9-49777b4306c8"
+  },
+  {
+    "img": "learn_2",
+    "title": "中班社会：大鼻子市长的烦恼",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/75a61674-a9e9-4ef5-89bc-9c04f14efdc1"
+  },
+  {
+    "img": "learn_3",
+    "title": "大班个别化学习：交通规则",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/21b2c622-f570-49b1-a80a-97b8c31db156"
+  },
+  {
+    "img": "game_1",
+    "title": "小班介入回应：撞车后……",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e767233e-180c-4ddd-9db2-41f6532320c6"
+  },
+  {
+    "img": "game_2",
+    "title": "中班分享交流：了不起的交警",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a49ebc5a-a249-4a52-b604-526e0e66ee5d"
+  },
+  {
+    "img": "game_3",
+    "title": "大班分享交流：拖车、汽车与飞机",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/d1c5d1e8-bee3-47b2-bf8a-2b76dd28c73f"
+  },
+  {
+    "img": "material_1",
+    "title": "主题素材包：各种各样的交通工具之汽车",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fa3a47d2-5411-40a6-81a9-486144a6e68e"
+  },
+  {
+    "img": "material_2",
+    "title": "主题素材包：各种各样的交通工具之自行车",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/37bdc21e-5a11-45fa-ba99-9da869d9be44"
+  },
+  {
+    "img": "material_3",
+    "title": "马路？高架！教案文档-压缩包",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9c864f9f-32b3-4d52-9f98-e3dbcbe92541"
+  }
+];
--- data/new_data.js
+++ data/new_data.js
@@ -3,92 +3,92 @@ const data = [
   {
     img: 'topic_right_1',
     title: '',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1e1292d1-cacd-4d06-97bc-d3899ea81189',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/823412d8-40e2-483f-906a-85422cef287b',
   },
   // 主题图 - 右下
   {
     img: 'topic_right_2',
     title: '',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fa4f3eeb-37d3-42cc-b0a4-c59e64f5c5e7',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/41b1e76c-0cdd-4771-b0ce-99b02286398f',
   },
   // 课程资源 - 生活板块 1/2/3
   {
     img: 'life_1',
-    title: '小班生活活动：老师在一起',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fb8c233e-95f4-45ff-864e-8bcd88e25654',
+    title: '小班生活活动：乐于分享',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/dbdd430d-bf06-4c7c-a734-1c5070366e9b',
   },
   {
     img: 'life_2',
-    title: '中班生活：马路安全我知道',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0f83f10e-3900-4d23-8f25-df0d752559c8',
+    title: '中班生活活动：一起去郊游',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/117531b8-d7de-4692-a963-d57d9f33aef1',
   },
   {
     img: 'life_3',
-    title: '大班环境创设：天天来园不迟到——早班车',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9a69256f-461c-49f1-958f-8be2c4bf8390',
+    title: '大班生活：我是小小百灵鸟',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/71dcddb1-4be4-42a0-9152-5336458b89b1',
   },
   // 课程资源 - 运动板块 1/2/3
   {
     img: 'sport_1',
-    title: '小班远足：会唱歌的车',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/cf2d5914-25ad-43ed-879a-ce36295d7a09',
+    title: '小班游戏玩法：欢乐蹦蹦跳',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/cad717cc-954c-4416-9ad6-655650d2ef0e',
   },
   {
     img: 'sport_2',
-    title: '中班远足：我在马路边',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/8a1fe2d8-5703-44ae-aaca-5c9a74d2e7f2',
+    title: '中班徒手体操：哈喽哈喽',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/60319d59-b16e-4b9f-b03a-cd409209415e',
   },
   {
     img: 'sport_3',
-    title: '大班活动案例：林中小路',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5c918ea0-9326-4601-8878-28e83783a5f8',
+    title: '大班体操：龟壳扭扭操',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/79915061-796f-43d0-9493-d4c2487b756d',
   },
   // 课程资源 - 学习板块 1/2/3
   {
     img: 'learn_1',
-    title: '小班科学：什么车子开来了',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a5e1f00c-0a6c-4f1c-bfd9-49777b4306c8',
+    title: '小班艺术活动：爱唱歌的猩猩',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/2fb39310-16d4-44dd-8803-2b215b180c4b',
   },
   {
     img: 'learn_2',
-    title: '中班社会：大鼻子市长的烦恼',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/75a61674-a9e9-4ef5-89bc-9c04f14efdc1',
+    title: '中班语言活动：梨子提琴',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a590dbe1-8583-415b-8a0b-33815ad2aa75',
   },
   {
     img: 'learn_3',
-    title: '大班个别化学习：交通规则',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/21b2c622-f570-49b1-a80a-97b8c31db156',
+    title: '大班艺术：自行车打击乐',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5d3f8b7d-6b40-4e52-9ebb-4a6ecd6af83e',
   },
   // 课程资源 - 游戏板块 1/2/3
   {
     img: 'game_1',
-    title: '小班介入回应：撞车后……',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e767233e-180c-4ddd-9db2-41f6532320c6',
+    title: '小班介入回应：歌表演《拔萝卜》如何引导幼儿“步调一致”',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/12049e0d-bd9a-4908-9b8a-26dd56fd2f41',
   },
   {
     img: 'game_2',
-    title: '中班分享交流：了不起的交警',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a49ebc5a-a249-4a52-b604-526e0e66ee5d',
+    title: '中班游戏中的故事：是谁在唱歌',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/900d2cbd-8b6a-4963-890a-2de3c867437c',
   },
   {
     img: 'game_3',
-    title: '大班分享交流：拖车、汽车与飞机',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/d1c5d1e8-bee3-47b2-bf8a-2b76dd28c73f',
+    title: '大班游戏中的故事：我能换乐器吗？',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/30ca1748-65e5-4f0b-811c-513ffeaa240e',
   },
   // 素材资源 1/2/3
   {
     img: 'material_1',
-    title: '主题素材包：各种各样的交通工具之汽车',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fa3a47d2-5411-40a6-81a9-486144a6e68e',
+    title: '中国功夫歌曲-压缩包',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a1ff16eb-43e1-4720-857a-771cd896d960',
   },
   {
     img: 'material_2',
-    title: '主题素材包：各种各样的交通工具之自行车',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/37bdc21e-5a11-45fa-ba99-9da869d9be44',
+    title: '音频素材：声音树',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/aa276e8a-f3a3-451a-87a7-be2e67be6224',
   },
   {
     img: 'material_3',
-    title: '马路？高架！教案文档-压缩包',
-    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9c864f9f-32b3-4d52-9f98-e3dbcbe92541',
+    title: '主题素材包：动物的本领之会唱歌的动物',
+    href: 'https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/51adfcc5-95e1-487b-a78b-35a0531d7762',
   },
 ];
