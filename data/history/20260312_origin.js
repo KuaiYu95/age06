5fcae79aa066768de7124333f664feee277ac1dc
yu.kuai <yu.kuai@chaitin.com>
2026-03-12 11:19:18 +0800

    [4]-3

--- data/history/20260312_origin.js
+++ data/history/20260312_origin.js
@@ -37,12 +37,12 @@ const data = [
   {
     "img": "topic_right_1",
     "title": "",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fb79cbc4-9698-4035-9e7b-4da83b2920ad"
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/49896f3f-e11a-4315-9779-1120a58d904b"
   },
   {
     "img": "topic_right_2",
     "title": "",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/31b398e4-6715-42ad-bb70-3949f4e5f39d"
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/92df1b8a-f937-4de7-87a5-97e9ec26427f"
   },
   {
     "img": "news_1",
@@ -141,18 +141,18 @@ const data = [
   },
   {
     "img": "life_1",
-    "title": "小班生活：早睡早起身体好",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/261b65e9-b28b-4508-a4ff-665656fc6fd3"
+    "title": "小班生活活动：乐于分享",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/dbdd430d-bf06-4c7c-a734-1c5070366e9b"
   },
   {
     "img": "life_2",
-    "title": "中班生活活动：我爱花草朋友",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/75d664ce-52e5-4409-96b0-f9d9290c3467"
+    "title": "中班生活：爸爸妈妈我爱你",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/430f735f-e459-4432-9dfe-3863c1856c65"
   },
   {
     "img": "life_3",
-    "title": "大班生活活动：花园里有什么",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/ce3839b9-2ab4-42f5-8d7e-78288485dde4"
+    "title": "大班环境创设：睡房我做主",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/0641699d-eea1-4788-bc12-98ad415e498a"
   },
   {
     "img": "sport_1",
@@ -161,57 +161,57 @@ const data = [
   },
   {
     "img": "sport_2",
-    "title": "中班区域性运动：竹的创玩",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/163bcb66-e8cf-4ab3-b4ca-61e8b8a46b5a"
+    "title": "中班运动活动：蹦蹦跳跳",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5feafc02-9b3e-4d67-ae40-ef44629185ea"
   },
   {
     "img": "sport_3",
-    "title": "大班户外运动：马到成功投壶套圈",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/ef913803-54ea-49a1-8fac-da6797b50594"
+    "title": "大班户外运动：快马加鞭踩脚印",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/ad90bc6a-9500-4461-a831-89995964493a"
   },
   {
     "img": "learn_1",
-    "title": "小班综合活动：小熊画大树",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/cc6e759d-223c-4f2b-b65a-61252a83e0e2"
+    "title": "小班探索活动：彩色光影窗贴",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9d1a5326-96a4-44d0-83de-3d83032f68a2"
   },
   {
     "img": "learn_2",
-    "title": "中班科学活动：走进万亩良田",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b7e05d94-c0c2-4d44-ac04-2e6358c2efc6"
+    "title": "中班民间游戏：马到成功蒙眼协作",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/bad71f42-8bfc-42b6-aa41-77823f76ae7f"
   },
   {
     "img": "learn_3",
-    "title": "大班科学活动：计划一次野营",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b2a7bec1-fa8f-4b77-af2f-830a918c7660"
+    "title": "大班艺术活动：瑞马欢腾创意偶戏",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e596578a-7f7a-4626-a200-c75237fd1d50"
   },
   {
     "img": "game_1",
-    "title": "小班户外：设置人工草坪支持多种游戏活动",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e226f76d-3680-4840-8659-6c699094d774"
+    "title": "混龄游戏环境：寻味新年游园活动",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/4dbce84c-6864-43aa-b1bf-8e137e7f78b6"
   },
   {
     "img": "game_2",
-    "title": "中班自然游戏环境创设：玩转树林",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/01fd0124-9507-4649-9218-b840f86fdc0e"
+    "title": "中班沙水游戏：马上有宝挖沙挑战",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/9d532a23-5fe9-4d71-9ccd-b01775277d84"
   },
   {
     "img": "game_3",
-    "title": "大班户外游戏环境创设：在草坪上玩建构游戏",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/f7f00b01-8cea-4850-a76e-8fd1edde630f"
+    "title": "大班自制玩教具：有趣的竹子",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/90981aad-e417-4b99-bdb4-75e708c66547"
   },
   {
     "img": "material_1",
-    "title": "数据赋能保健，除了晨检还能做什么？",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b9318a3e-0948-46b6-8de1-b3b487ce863e"
+    "title": "在自然探索中认知世界",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fd66e90d-a6c3-412b-94a7-d39467082f06"
   },
   {
     "img": "material_2",
-    "title": "智能晨检助力传染病防控",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a0cf5754-324a-43d4-8d16-72548aa47b43"
+    "title": "活动室的墙",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e7eb0827-95b7-425f-8966-dea987c67ace"
   },
   {
     "img": "material_3",
-    "title": "数据赋能支持每一个幼儿健康成长",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5d516fc4-558b-46ab-8c2e-4dea848168fe"
+    "title": "在社会情境中感知生活",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/f0de2f97-a4de-452e-a049-ce40bd954cf1"
   }
 ];
--- data/new_data.js
+++ data/new_data.js
@@ -1,7 +1,87 @@
 const data = [
   {
-    "img": "class_3",
-    "title": "大班谈话活动：我的名字叫“建国”",
-    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1252b6f1-3e76-40a2-adbe-0ac3c24400f5"
+    "img": "topic_right_1",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/fb79cbc4-9698-4035-9e7b-4da83b2920ad"
   },
+  {
+    "img": "topic_right_2",
+    "title": "",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/31b398e4-6715-42ad-bb70-3949f4e5f39d"
+  },
+  {
+    "img": "life_1",
+    "title": "小班生活：早睡早起身体好",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/261b65e9-b28b-4508-a4ff-665656fc6fd3"
+  },
+  {
+    "img": "life_2",
+    "title": "中班生活活动：我爱花草朋友",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/75d664ce-52e5-4409-96b0-f9d9290c3467"
+  },
+  {
+    "img": "life_3",
+    "title": "大班生活活动：花园里有什么",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/ce3839b9-2ab4-42f5-8d7e-78288485dde4"
+  },
+  {
+    "img": "sport_1",
+    "title": "小班户外运动：小马向前冲",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/1d1bb2ac-944c-4084-82bc-0c5fe5f9cfe9"
+  },
+  {
+    "img": "sport_2",
+    "title": "中班区域性运动：竹的创玩",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/163bcb66-e8cf-4ab3-b4ca-61e8b8a46b5a"
+  },
+  {
+    "img": "sport_3",
+    "title": "大班户外运动：马到成功投壶套圈",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/ef913803-54ea-49a1-8fac-da6797b50594"
+  },
+  {
+    "img": "learn_1",
+    "title": "小班综合活动：小熊画大树",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/cc6e759d-223c-4f2b-b65a-61252a83e0e2"
+  },
+  {
+    "img": "learn_2",
+    "title": "中班科学活动：走进万亩良田",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b7e05d94-c0c2-4d44-ac04-2e6358c2efc6"
+  },
+  {
+    "img": "learn_3",
+    "title": "大班科学活动：计划一次野营",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b2a7bec1-fa8f-4b77-af2f-830a918c7660"
+  },
+  {
+    "img": "game_1",
+    "title": "小班户外：设置人工草坪支持多种游戏活动",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/e226f76d-3680-4840-8659-6c699094d774"
+  },
+  {
+    "img": "game_2",
+    "title": "中班自然游戏环境创设：玩转树林",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/01fd0124-9507-4649-9218-b840f86fdc0e"
+  },
+  {
+    "img": "game_3",
+    "title": "大班户外游戏环境创设：在草坪上玩建构游戏",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/f7f00b01-8cea-4850-a76e-8fd1edde630f"
+  },
+  {
+    "img": "material_1",
+    "title": "数据赋能保健，除了晨检还能做什么？",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/b9318a3e-0948-46b6-8de1-b3b487ce863e"
+  },
+  {
+    "img": "material_2",
+    "title": "智能晨检助力传染病防控",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/a0cf5754-324a-43d4-8d16-72548aa47b43"
+  },
+  {
+    "img": "material_3",
+    "title": "数据赋能支持每一个幼儿健康成长",
+    "href": "https://yyt.age06.edu.sh.cn/Age06.ImplementSupport/Resource/Detail/5d516fc4-558b-46ab-8c2e-4dea848168fe"
+  }
 ];
